382e68efdda40c9a321e4df34326a046077197a4
leungj <jerry.leung.osu@gmail.com>
2014-11-19 03:22:37 -0800

    Title Screen Done Commit

--- app/src/main/java/com/jerryleung/utap/MainActivity.java
+++ app/src/main/java/com/jerryleung/utap/MainActivity.java
@@ -1,9 +1,11 @@
 package com.jerryleung.utap;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainActivity extends Activity {
@@ -35,5 +37,11 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void prevention_click(View view) {
+        Intent getPreventionScreenIntent = new Intent(this, PreventionScreen.class);
+        final int result = 1;
+        startActivity(getPreventionScreenIntent);
     }
 }
---
+++ app/src/main/java/com/jerryleung/utap/PreventionScreen.java
@@ -0,0 +1,7 @@
+package com.jerryleung.utap;
+
+/**
+ * Created by jerryleung on 11/19/14.
+ */
+public class PreventionScreen {
+}
